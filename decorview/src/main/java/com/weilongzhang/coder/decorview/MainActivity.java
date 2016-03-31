8ad174b4aa921e4847a3370a0b44e399b8e51d31
zhangweilong <weilongzhang16@gmail.com>
2016-03-31 19:37:13 +0800

    decorview height

---
+++ decorview/src/main/java/com/weilongzhang/coder/decorview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.weilongzhang.coder.decorview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus){
+            Log.d("decorView:height:",this.getWindow().getDecorView().getHeight()+"");
+        }
+    }
+}
