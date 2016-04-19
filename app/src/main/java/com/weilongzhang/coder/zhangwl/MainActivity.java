7d007ce9d41cd21d64d4b68d013075ee839e6ed0
zhangweilong <weilongzhang16@gmail.com>
2016-04-19 19:21:54 +0800

    另一种pc的实现

--- app/src/main/java/com/weilongzhang/coder/zhangwl/MainActivity.java
+++ app/src/main/java/com/weilongzhang/coder/zhangwl/MainActivity.java
@@ -1,5 +1,6 @@
 package com.weilongzhang.coder.zhangwl;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,12 +8,13 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.weilongzhang.coder.zhangwl.customview.ProgressCircle;
-import com.weilongzhang.coder.zhangwl.customview.ProgressCircle.OnProgressChangeListener;
+import com.weilongzhang.coder.zhangwl.customview.ProgressCircleView;
+import com.weilongzhang.coder.zhangwl.customview.ProgressCircleView2;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ProgressCircle progressCircle;
+    private ProgressCircleView progressCircle;
+    private ProgressCircleView2 pc2;
     private TextView textView;
 
     @Override
@@ -20,16 +22,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView =  (TextView) this.findViewById(R.id.tv_onclick);
+        pc2 = (ProgressCircleView2) findViewById(R.id.pc2);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"textview click!!!",Toast.LENGTH_LONG).show();
             }
         });
-        progressCircle = (ProgressCircle) this.findViewById(R.id.rp);
+        progressCircle = (ProgressCircleView) this.findViewById(R.id.rp);
         progressCircle.setProgress(99);
         progressCircle.setCanAnimation(true);
-        progressCircle.setOnProgressChangeListener(new OnProgressChangeListener() {
+        progressCircle.setOnProgressChangeListener(new ProgressCircleView.OnProgressChangeListener() {
             @Override
             public void progressChanging(String percent) {
                 Log.d("",percent);
@@ -45,6 +48,21 @@ public class MainActivity extends AppCompatActivity {
                 return super.modifyValue(value);
             }
         });
+
+
+
+        final int rightRate = 80;
+        ValueAnimator mValueAnimator = ValueAnimator.ofFloat(0f, 1.0f);
+        mValueAnimator.setDuration(500);
+        mValueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float value = (Float) valueAnimator.getAnimatedValue();
+                int rate = (int) (rightRate * value);
+                pc2.setAccuracy(rate);
+            }
+        });
+        mValueAnimator.start();
     }
 
     public void refresh(View view){
